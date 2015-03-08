3fbe47ed62054983377b0822991e07329dc72934
Jacob Baum <jacobbaum@gmail.com>
2015-03-07 21:22:31 -0500

    Happy path methods written. User journeys & posts can be created in one step if methods are chained.

--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -1,34 +1,69 @@
 module Instagram
   @access_token = "295738699.1fb234f.fda44f9e854f4083acc3b024e883aeaa"
-  @tag_json = Hash.new
-
-  # def self.get_tag(tag)
-  #   HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{@access_token}")
-  # end
+  @tags_json = Hash.new
+  @user_posts = Array.new
+  @current_user = User.new
+ 
+  # TODO method to get a new user's Instagram id number
+  # user search url, for finding a user's id:
+  # https://api.instagram.com/v1/users/search?q=jack&access_token=ACCESS-TOKEN
 
   def self.get_locative
-    @tag_json = HTTParty.get("https://api.instagram.com/v1/tags/locative/media/recent?access_token=#{@access_token}")
+    tags = ['locativego', 'locative', 'locativeend']
+    tags.each do |tag|
+      @tags_json[tag] = HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{@access_token}")
+    end
     return self
   end
 
-  def self.collect_user_posts(instagram_id)
-    locative_posts = Array.new
-    @tag_json['data'].each do |post|
-      locative_posts << post if post['user']['id'].to_i == instagram_id
+  def self.collect_user_posts(user)
+    @current_user = user
+    @tags_json.each do |tag, hash|
+      hash['data'].each do |post|
+        @user_posts << post if post['user']['id'] == @current_user.instagram_id.to_s
+      end
     end
-    return locative_posts
+    return self
   end
-    
-  def self.filter_json
+  
+  def self.make_journeys
+    current_journey = @current_user.journeys.last
+    @user_posts.sort_by! { |post| post['created_time'] }
+    @user_posts.each do |post|
+      case 
+      when post['tags'].include?('locativego')
+        current_journey = @current_user.journeys.create(
+        name: "New Journey",
+        date: Time.at(post['created_time'].to_i).strftime("%A, %B %d, %Y"))
+        add_post_to_journey(current_journey, post)
+      when post['tags'].include?('locative')
+        add_post_to_journey(current_journey, post)
+      when post['tags'].include?('locativeend')
+        add_post_to_journey(current_journey, post)      
+      end
+    end
   end
 
+  def self.add_post_to_journey(journey, post)
+    journey.posts.create(
+      caption:      post['caption']['text'],
+      latitude:     post['location']['latitude'].to_f,
+      longitude:    post['location']['longitude'].to_f,
+      time_stamp:   post['created_time'],
+      tags:         post['tags'],
+      low_res_img:  post['images']['low_resolution']['url'],
+      med_res_img:  post['images']['thumbnail']['url'],
+      hi_res_img:   post['images']['standard_resolution']['url'])
+  end
 
   def self.say_hello
     "Hello, I'm Instagram"
   end
 
-# user search url, for finding a user's id when they sign in
-# https://api.instagram.com/v1/users/search?q=jack&access_token=ACCESS-TOKEN
 end
+
+
+
+
 
 
