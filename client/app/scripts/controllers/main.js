51e7c084613c85ed9bd73774ed9bfc3b09a37038
Ric Cabrera <wawashorecords@gmail.com>
2015-03-08 16:50:52 -0400

    Added markers

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name locativeApp.controller:MainCtrl
@@ -9,9 +8,36 @@
  */
 angular.module('locativeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    
+    $scope.map = {
+      center: {
+        latitude: 40.1451,
+        longitude: -99.6680
+      },
+      zoom: 4,
+      bounds: {}
+    };
+
+    $scope.markerList = [
+      {
+        id: 0,
+        latitude: 45,
+        longitude: -73
+      },
+      {
+        id: 1,
+        latitude: 46,
+        longitude: -74
+      },
+      {
+        id: 2,
+        latitude: 48,
+        longitude: -75
+      },
+      {
+        id: 3,
+        latitude: 49,
+        longitude: -76
+      },
     ];
   });
