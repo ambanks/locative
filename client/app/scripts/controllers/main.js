e748a24772adb5dc77a3302c7850135e948683d7
Ric Cabrera <wawashorecords@gmail.com>
2015-03-10 12:18:19 -0400

    Created HTTP request to server

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -7,8 +7,8 @@
  * Controller of the locativeApp
  */
 angular.module('locativeApp')
-  .controller('MainCtrl', function ($scope) {
-    
+  .controller('MainCtrl', function ($scope, $http) {
+
     $scope.map = {
       center: {
         latitude: 40.1451,
@@ -18,38 +18,40 @@ angular.module('locativeApp')
       bounds: {}
     };
 
-    $scope.polylines = [
-      {
-        id: 1,
-        path: [
-            {
-                latitude: 33.7335,
-                longitude: -84.3735
+    $http.get('/api/users/4/journeys/13/posts')
+      .success(function(data) {
+        console.log(data);
+        $scope.polylines = [
+          {
+            id: 1,
+            path: [
+                {
+                    latitude: data[0]['latitude'],
+                    longitude: data[0]['longitude']
+                },
+                {
+                    latitude: data[1]['latitude'],
+                    longitude: data[1]['longitude']
+                },
+
+            ],
+            stroke: {
+                color: '#6060FB',
+                weight: 3
             },
-            {
-                latitude: 33.7487,
-                longitude: -84.3713
-            },
-            {
-                latitude: 33.7729,
-                longitude: -84.3657
-            }  
-        ],
-        stroke: {
-            color: '#6060FB',
-            weight: 3
-        },
-        editable: true,
-        draggable: true,
-        geodesic: true,
-        visible: true,
-        icons: [{
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE
-            },
-            offset: '25px'
-          
-        }]
-      }
-    ];
+            editable: true,
+            draggable: true,
+            geodesic: true,
+            visible: true,
+            icons: [{
+                icon: {
+                    path: google.maps.SymbolPath.CIRCLE
+                },
+                offset: '25px'
+              
+            }]
+          }
+        ];
+      });
+
   });
