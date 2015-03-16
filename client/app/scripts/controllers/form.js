404578320ac14d985609c09112ee404d502057f9
Jacob Baum <jacobbaum@gmail.com>
2015-03-16 19:35:23 -0400

    Basic authorization in place. Users can only edit and delete their own journeys and posts.

--- client/app/scripts/controllers/form.js
+++ client/app/scripts/controllers/form.js
@@ -5,6 +5,35 @@ angular.module('locativeApp')
   ['$scope', '$rootScope', '$state', 'AuthService', 'JourneyService',
   function ($scope, $rootScope, $state, AuthService, JourneyService) {
 
+  $scope.logUsers = function(){
+    console.log('User: ' + JSON.stringify($scope.user));
+    console.log('Current User: ' + JSON.stringify($scope.currentUser));
+  };
+
+    console.log('User: ' + JSON.stringify($scope.user));
+    console.log('Current User: ' + JSON.stringify($scope.currentUser));
+
+  $scope.isAuthorized = function() {
+    var userId = $scope.user.id;
+    var currentUserId = $scope.currentUser.id;
+    return userId == currentUserId;
+  };
+
+
+
+  function isAuthorized() {
+    var userId = $scope.user.id;
+    var currentUserId = $scope.currentUser.id;
+    return userId == currentUserId;
+    // return $scope.user === $scope.currentUser;
+  }
+
+
+
+
+
+  console.log(isAuthorized());
+
   $scope.updateJourney = function(journey) {
     return JourneyService.updateJourney(journey)
     .success(function() {
